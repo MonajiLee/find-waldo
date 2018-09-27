05773eae2effd2e967d32a9984092e96300d16ff
MonajiLee <lee.smonica@gmail.com>
2018-09-27 19:44:01 +0000

    Refactored to use forEach method

--- find-waldo.js
+++ find-waldo.js
@@ -1,20 +1,40 @@
 // The second argument/parameter is expected to be a function
 
-function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
-        found([i]);   // execute callback
-      }
-    }
-  }
+// 2. Refactor the function findWaldo to use the forEach() method instead of a for loop.
 
-//   Modify the callback function in the previous example so that logs the index of the array 
+// arr = ["Alice", "Bob", "Waldo", "Winston"];
+
+function findWaldo(arr, cb){
+    arr.forEach(function (element, i) {
+        if (element === "Waldo") {
+            cb(i);   // execute callback
+        }  
+    })
+}
+
+// SECOND ATTEMPT AT .forEach
+// list.forEach(function findWaldo(i){
+//     if ([i] === "Waldo") {
+//         found([i]);   // execute callback
+//       }
+// });
+
+
+// FIRST ATTEMPT AT .forEach
+// function findWaldo.forEach(arr, found) {
+//       if (arr[i] === "Waldo") {
+//         found([i]);   // execute callback
+//       }
+// };
+
+
+//   1. Modify the callback function in the previous example so that logs the index of the array 
 //   where Waldo is found, ie. "Found Waldo at index 2!". 
 //   (You will need to modify actionWhenFound to receive the index.)
 
-function actionWhenFound([i]) {
-    
-    console.log("Found Waldo at index " + [i]);
+function actionWhenFound(i) {
+    console.log("Found Waldo at index " + i);
   }
-  
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+findWaldo(["Alice", "Bob", "Mary", "Waldo", "Winston"], actionWhenFound);
