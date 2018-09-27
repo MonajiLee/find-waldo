de408c810565b35a0367cf36ca24e97c9ae60520
MonajiLee <lee.smonica@gmail.com>
2018-09-27 15:17:45 +0000

    Print the index of Waldo when found

--- find-waldo.js
+++ find-waldo.js
@@ -3,13 +3,18 @@
 function findWaldo(arr, found) {
     for (var i = 0; i < arr.length; i++) {
       if (arr[i] === "Waldo") {
-        found();   // execute callback
+        found([i]);   // execute callback
       }
     }
   }
-  
-function actionWhenFound() {
-    console.log("Found him!");
+
+//   Modify the callback function in the previous example so that logs the index of the array 
+//   where Waldo is found, ie. "Found Waldo at index 2!". 
+//   (You will need to modify actionWhenFound to receive the index.)
+
+function actionWhenFound([i]) {
+    
+    console.log("Found Waldo at index " + [i]);
   }
   
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
